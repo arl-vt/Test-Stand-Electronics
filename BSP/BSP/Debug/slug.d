89b165ca04e5825b32128843d32326f4a2467c60
Shriya <sshriya7@vt.edu>
2017-05-18 18:59:47 -0400

    Added BSP
    GPIO, Motor

---
+++ BSP/BSP/Debug/slug.d
@@ -0,0 +1,19 @@
+# FIXED
+
+slug.obj: ../slug.c
+slug.obj: ../slug.h
+slug.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.3.0.STS/include/stdint.h
+slug.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.3.0.STS/include/stdbool.h
+slug.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+slug.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+slug.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+slug.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+
+../slug.c: 
+../slug.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.3.0.STS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.3.0.STS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
---
+++ BSP/BSP/Debug/slugTest.d
@@ -0,0 +1,9 @@
+# FIXED
+
+slugTest.obj: ../slugTest.c
+slugTest.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.3.0.STS/include/stdint.h
+slugTest.obj: ../slug.h
+
+../slugTest.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.3.0.STS/include/stdint.h: 
+../slug.h: 
